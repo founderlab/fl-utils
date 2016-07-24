a4913e7d3b5a644497af78c22d0ee32cef39d5b3
gwilym <gwilym@gwilymh.com>
2016-07-25 09:23:32 +1000

    fetchComponentData takes a parallelism option

--- src/fetchComponentData.js
+++ src/fetchComponentData.js
@@ -1,10 +1,14 @@
 import _ from 'lodash' // eslint-disable-line
 import Queue from 'queue-async'
 
+const defaults = {
+  parallelism: 1,
+}
+
 export default function fetchComponentData(options, callback) {
-  const {store, components, action} = options
+  const {store, components, action, parallelism} = _.defaults(options, defaults)
   const result = {}
-  const queue = new Queue()
+  const queue = new Queue(parallelism)
 
   components.forEach(_Component => {
     if (!_Component) return
